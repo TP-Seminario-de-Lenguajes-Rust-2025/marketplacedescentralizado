c7f09f7e62a378001c6540ef29045be337f6bc2e
NetCreature <agustinmediotti@gmail.com>
2025-07-12 21:01:15 -0300

    removed dependencies and refactored ids

--- lib.rs
+++ lib.rs
@@ -1,96 +1,70 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-
 #[ink::contract]
 mod contract {
-    
-    use ink::{prelude::{string::String,vec::Vec},storage::{Mapping, StorageVec}};
-    //use crate::estructuras::{publicacion::*,producto::*,orden::*,usuario::*};
-    use uuid::Uuid; //borrar no usamos mas ahora usamos len()+1
-    use std::fmt;
+    use ink::{
+        prelude::{string::String, vec::Vec},
+        storage::{Mapping, StorageVec},
+    };
 
-    const COMPRADOR : &str = "1";
-    const VENDEDOR : &str = "2";
+    const COMPRADOR: u32 = 1;
+    const VENDEDOR: u32 = 2;
 
     ///LOGICA DE ORDEN
-    /// 
-    
+    ///
 
-    #[cfg_attr(
-        feature = "std",
-        derive(ink::storage::traits::StorageLayout)
-    )]
-    #[derive(Debug)]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
-
-    #[derive(Clone, Copy, PartialEq)]
-    #[allow(dead_code)]
     pub enum ErroresApp {
-        ErrorComun
+        ErrorComun,
     }
 
-    impl fmt::Display for ErroresApp {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            match self {
-                ErroresApp::ErrorComun => write!(f, "PARA LOCO"),
-            }
-        }
-    }
-
-
-    #[cfg_attr(
-        feature = "std",
-        derive(ink::storage::traits::StorageLayout)
-    )]
-    #[derive(Debug)]
+    #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
-
-    #[derive(Clone, Copy, PartialEq)]
-    #[allow(dead_code)]
     pub enum EstadoOrden {
         Pendiente,
-        Enviada, //solo lo puede modificar el vendedor
-        Recibida, //solo lo puede modificar el comprador
+        Enviada,   //solo lo puede modificar el vendedor
+        Recibida,  //solo lo puede modificar el comprador
         Cancelada, //tienen que estar ambos de acuerdo y tiene que estar en estado pendiente
     }
 
-    #[cfg_attr(
-        feature = "std",
-        derive(ink::storage::traits::StorageLayout)
-    )]
-    #[derive(Debug)]
+    #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
     pub struct Orden {
         //info de la orden
-        id: String,
-        id_vendedor: String,
-        id_comprador: String,
+        id: u32,
+        id_vendedor: u32,
+        id_comprador: u32,
         status: EstadoOrden,
-        productos: Vec<String>, //vec con uuid de los productos
+        productos: Vec<u32>,       //vec con uuid de los productos
         cal_vendedor: Option<u8>,  //calificacion que recibe el vendedor
         cal_comprador: Option<u8>, //calificacion que recibe el comprador
     }
 
-    impl Default for Orden{
+    impl Default for Orden {
         fn default() -> Self {
-            Orden{
-                id: "".to_string(),
-                id_vendedor: "".to_string(),
-                id_comprador: "".to_string(),
+            Orden {
+                id: 0,
+                id_vendedor: 0,
+                id_comprador: 0,
                 status: EstadoOrden::Pendiente,
                 productos: Vec::new(),
                 cal_vendedor: None,
-                cal_comprador: None, 
+                cal_comprador: None,
             }
         }
     }
 
     impl Orden {
-    }
         //nuevo new de orden sin usar uuid pasamos id desde el sistema
-        pub fn new(id: String, id_vendedor:String, id_comprador:String, productos:Vec<String>) -> Orden {
-            Orden { id, id_vendedor, id_comprador, productos, ..Default::default() }
+        pub fn new(id: u32, id_vendedor: u32, id_comprador: u32, productos: Vec<u32>) -> Orden {
+            Orden {
+                id,
+                id_vendedor,
+                id_comprador,
+                productos,
+                ..Default::default()
             }
+        }
 
         //pub fn cambiar_estado
         //fn set_enviada() //solamente puede ser modificada por el vendedor
@@ -99,16 +73,12 @@ mod contract {
     }
 
     /// LOGICA DE PRODUCTO
-    /// 
+    ///
 
-    #[cfg_attr(
-        feature = "std",
-        derive(ink::storage::traits::StorageLayout)
-    )]
-    #[derive(Debug)]
+    #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
     pub struct Categoria {
-        id: String,
+        id: u32,
         nombre: String,
     }
 
@@ -118,42 +88,36 @@ mod contract {
         }
     }
 
-    #[cfg_attr(
-        feature = "std",
-        derive(ink::storage::traits::StorageLayout)
-    )]
-    #[derive(Debug)]
+    #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
     pub struct Producto {
-        id: String,
+        id: u32,
         nombre: String,
-        desc: String,
-        precio: String,
-        categ: String,
+        descripcion: String,
+        categoria: u32,
     }
 
     impl Producto {
-
-        pub fn new(id: String, nombre: String, descripcion:String, precio:String, categoria: String) -> Producto {
+        pub fn new(id: u32, nombre: String, descripcion: String, categoria: u32) -> Producto {
             //TODO: verificar que stock>0 y precio>0 y nombre y desc sean validos
-            Producto { id, nombre, desc: descripcion, precio, categ: categoria }
-            
+            Producto {
+                id,
+                nombre,
+                descripcion,
+                categoria,
+            }
         }
     }
 
     ///LOGICA DE PUBLICACION
-    
-    #[cfg_attr(
-        feature = "std",
-        derive(ink::storage::traits::StorageLayout)
-    )]
-    #[derive(Debug)]
+
+    #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
     pub struct Publicacion {
-        id: String,
-        id_prod: String, //id del producto que contiene
-        id_user: String, //id del user que publica
-        stock: u8,
+        id: u32,
+        id_prod: u32, //id del producto que contiene
+        id_user: u32, //id del user que publica
+        stock: u32,
         activa: bool,
     }
 
@@ -165,30 +129,28 @@ mod contract {
         pub fn is_activa(&self) -> bool {
             self.activa
         }
-        
-        pub fn actualizar_stock(&mut self, delta: i32) -> Result<(), ErroresApp> {
-        
-        }
 
+        // pub fn actualizar_stock(&mut self, delta: i32) -> Result<(), ErroresApp> {}
 
-        
         //nueva implementacion del new de publicacion sin usar uuid
-        pub fn new(id: String, id_producto: String, id_user: String, stock: u8) -> Publicacion {
-            Publicacion { id, id_prod: id_producto, id_user, stock, activa: true }
+        pub fn new(id: u32, id_producto: u32, id_user: u32, stock: u32) -> Publicacion {
+            Publicacion {
+                id,
+                id_prod: id_producto,
+                id_user,
+                stock,
+                activa: true,
+            }
         }
     }
 
     /// LOGICA DE DE USUARIO
-    /// 
-    
-    #[cfg_attr(
-        feature = "std",
-        derive(ink::storage::traits::StorageLayout)
-    )]
-    #[derive(Debug)]
+    ///
+
+    #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
     pub struct Rol {
-        id: String, //not a Uuid
+        id: u32, //not a Uuid
         desc: String,
     }
 
@@ -198,21 +160,19 @@ mod contract {
         }
     }
 
-
-    #[cfg_attr(
-        feature = "std",
-        derive(ink::storage::traits::StorageLayout)
-    )]
-    #[derive(Debug)]
+    #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
     struct Rating {
-        calificacion_comprador: (u16, u32), //cant de compras, valor cumulativo de todas las calificaciones
-        calificacion_vendedor: (u16, u32),
+        calificacion_comprador: (u32, u32), //cant de compras, valor cumulativo de todas las calificaciones
+        calificacion_vendedor: (u32, u32),
     }
 
     impl Rating {
-        fn new() -> Rating{
-            Rating { calificacion_comprador: (0,0), calificacion_vendedor: (0,0) }
+        fn new() -> Rating {
+            Rating {
+                calificacion_comprador: (0, 0),
+                calificacion_vendedor: (0, 0),
+            }
         }
         fn get_rating_comprador() -> f64 {
             todo!("debe devolver Result<f64,ErrDivisionZero>")
@@ -223,28 +183,29 @@ mod contract {
         }
     }
 
-
-    #[cfg_attr(
-        feature = "std",
-        derive(ink::storage::traits::StorageLayout)
-    )]
-    #[derive(Debug)]
+    #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
     pub struct Usuario {
-        id: String,
+        id: u32,
         nombre: String,
         mail: String,
         rating: Rating,
-        roles: Vec<String> //id de rol
+        roles: Vec<u32>, //id de rol
     }
 
     impl Usuario {
-        pub fn new(id: String, nombre: String, mail: String, roles: Vec<String>) -> Usuario {
-            Usuario { id, nombre, mail, rating: Rating::new(), roles }
+        pub fn new(id: u32, nombre: String, mail: String, roles: Vec<u32>) -> Usuario {
+            Usuario {
+                id,
+                nombre,
+                mail,
+                rating: Rating::new(),
+                roles,
+            }
         }
 
-        pub fn has_role(&self, rol:&str) ->bool{
-            self.roles.contains(&rol.to_string())
+        pub fn has_role(&self, rol: u32) -> bool {
+            self.roles.contains(&rol)
         }
 
         pub fn get_rating_comprador() -> f64 {
@@ -254,25 +215,16 @@ mod contract {
             todo!()
         }
 
-        pub fn agregar_rol(){
-            
-        }
+        pub fn agregar_rol() {}
     }
-   
+
     #[ink(storage)]
     pub struct Sistema {
         users: Vec<Usuario>,
-
-        // //asociacion entre usuario y rol
-        roles: Mapping<String, Rol>, //roles que existen
-        
-        categorias: Mapping<String, Categoria>, //id_categ
-
+        roles: Mapping<u32, Rol>,             //roles que existen
+        categorias: Mapping<u32, Categoria>,  //id_categ
         ordenes_historico: StorageVec<Orden>, //registro de compras
-
         productos: StorageVec<Producto>,
-
-        // //guarda las publicaciones
         publicaciones: Vec<Publicacion>, //capaz no un vec
     }
 
@@ -282,7 +234,6 @@ mod contract {
             todo!()
             //Sistema {}
         }
-
 
         /// role related
         pub fn asociar_rol() {
@@ -294,67 +245,88 @@ mod contract {
         }
 
         #[ink(message)]
-        pub fn registrar_usuario(id:u32, nombre: String, mail: String, roles: Vec<String>) -> Result<String, ErroresApp> {
+        pub fn registrar_usuario(
+            &mut self,
+            nombre: String,
+            mail: String,
+            roles: Vec<u32>,
+        ) -> Result<u32, ErroresApp> {
             //verifico que el email no este asosiado a otra cuenta (que no esta repetido)
             if self.users.iter().any(|u| u.mail == mail) {
                 return Err(ErroresApp::ErrorComun);
             }
-            let id = (self.users.len() + 1).to_string();
-            let nuevo_usuario = Usuario::new(id.clone(), nombre, mail, roles);
-            self.users.push(nuevo_usuario);
-            //si todo sale bien devuelvo el id del nuevo user
-            Ok(id)
+            if let Some(id) = self.users.len().checked_add(1) {
+                let nuevo_usuario = Usuario::new(id as u32, nombre, mail, roles);
+                self.users.push(nuevo_usuario);
+                Ok(id as u32)
+            } else {
+                Err(ErroresApp::ErrorComun) // maquetar error
+            }
         }
 
-
         #[ink(message)]
-        pub fn crear_publicacion(id:u32, nombre: String, descripcion:String, precio:String,stock: u8,categoria:String, id_user:String){
-            if let Some(user) = self.users.iter().find(|u|u.id==id_user){
-                if user.roles.contains(&VENDEDOR.to_string()){
-                    if let Ok(prod) = self.crear_producto(nombre, descripcion, precio, categoria){
-                        //nueva implementacion para no usar uuid
-                        let id = (self.publicaciones.len() + 1).to_string(); 
-                        let p = Publicacion::new(id, prod.id, id_user, stock);
-                        self.publicaciones.push(p);
-                    };
-                }else{
-
+        pub fn crear_publicacion(
+            &mut self,
+            id: u32,
+            nombre: String,
+            descripcion: String,
+            precio: String,
+            stock: u32,
+            categoria: u32,
+            id_user: u32,
+        ) {
+            if let Some(user) = self.users.iter().find(|u| u.id == id_user) {
+                if user.roles.contains(&VENDEDOR) {
+                    // if let Ok(prod) = self.crear_producto(id, nombre, descripcion, categoria) {
+                    //     let p = Publicacion::new(id, prod.id, id_user, stock);
+                    //     self.publicaciones.push(p);
+                    // }; Se tiene que crear aparte.
                 }
-            }else{
-                todo!("error")
+            } else {
+                todo!("error");
             }
         }
 
-        
-        fn crear_producto(id:u32, nombre:String,descripcion:String,precio:String, categoria:String) -> Result<Producto,ErroresApp>{
-            if self.categorias.try_get(categoria.clone()).is_some(){//verifica la categoria
-                //nueva implementacion para no usar uuid
-                let p = Producto::new(id, nombre, descripcion, precio, categoria);
-                self.productos.push(&p);//??
-                Ok(p)
-            }else{
-                todo!("aca a")
+        fn crear_producto(
+            &self,
+            id: u32,
+            nombre: String,
+            descripcion: String,
+            categoria: u32,
+        ) -> Producto {
+            if self.categorias.try_get(categoria).is_some() {
+                Producto::new(id, nombre, descripcion, categoria);
+                // let producto = Producto::new(id, nombre, descripcion, categoria);
+                // return Ok(producto);
             }
+            todo!("-> Result<Producto, ErroresApp>");
         }
 
         #[ink(message)]
-        pub fn crear_orden(id:32, id_vendedor:String, id_comprador:String, productos:Vec<String>){
-            if let Some(comprador) = self.users.iter().find(|u|u.id==id_comprador){ //verifica que existe el usuario
-                if let Some(vendedor) = self.users.iter().find(|u|u.id==id_vendedor){
-                    if comprador.has_role(COMPRADOR) && vendedor.has_role(VENDEDOR){ //verifica que tienen los roles necesarios
-
-                        //nueva implementacion para no usar uuid
-                    /*  let id = (self.ordenes_historico.len() + 1).to_string();
-                        let o = Orden::new(id, id_vendedor, id_comprador, productos.clone());
-                         */
-                        for prod in productos.iter(){
-                            if let Some(publi) = self.publicaciones.iter_mut().find(|p|*p.id_prod==*prod){
-                                if publi.stock>0{
-                                    //publi.stock-=1; //error: arithmetic operation that can potentially result in unexpected side-effects
+        pub fn crear_orden(
+            &mut self,
+            id: u32,
+            id_vendedor: u32,
+            id_comprador: u32,
+            productos: Vec<u32>,
+        ) {
+            if let Some(comprador) = self.users.iter().find(|u| u.id == id_comprador) {
+                //verifica que existe el usuario
+                if let Some(vendedor) = self.users.iter().find(|u| u.id == id_vendedor) {
+                    if comprador.has_role(COMPRADOR) && vendedor.has_role(VENDEDOR) {
+                        //verifica que tienen los roles necesarios
+                        for prod in productos.iter() {
+                            if let Some(publi) =
+                                self.publicaciones.iter_mut().find(|p| p.id_prod == *prod)
+                            {
+                                if publi.stock > 0 {
+                                    todo!("Crear una funcion de Sistema para reducir el stock");
                                 }
                             }
                         }
-                        self.ordenes_historico.push(&o);//??
+                        let orden = Orden::new(id, id_vendedor, id_comprador, productos);
+                        todo!("Generar orden y pusherarla a Sistema");
+                        // self.ordenes_historico.push(&o); ??
                     }
                 }
             }
